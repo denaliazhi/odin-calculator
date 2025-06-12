4596304ebec032bcb42ad15618b6d5893e9cd2eb
Denalia Zhi <d.naliazhi@gmail.com>
2025-06-12 17:00:09 -0400

    Allow user to backspace or input decimal, Round results to 4 places, Change some class names

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const userPicked = [];
 
+const display = document.querySelector(".display");
+const output = document.createElement("p");
+
 const digits = document.querySelector(".digits");
 digits.addEventListener("click", updateNumber);
 
 const operators = document.querySelector(".operators");
 operators.addEventListener("click", updateOperator);
 
-const general = document.querySelector(".general");
-general.addEventListener("click", (e) => {
-    if (e.target.parentNode.className != 'keypad') {
-        const choice = e.target.textContent;
-        if (choice == '=') {
-            getResult(e);
-        } else {
-            clearAll();
-            updateDisplay();
-        }
-    }
-}
-);
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", getResult);
 
-const display = document.querySelector(".display");
-const output = document.createElement("p");
+const clear = document.querySelector(".clear-all");
+clear.addEventListener("click", () => {
+    clearAll();
+    updateDisplay();
+})
+
+const clearEntry = document.querySelector(".clear-entry");
+clearEntry.addEventListener("click", clearOne);
+
+const decimal = document.querySelector(".separator");
+decimal.addEventListener("click", addDecimal);
 
 function updateNumber(e) {
     if (e.target.parentNode.className != 'keypad') {
@@ -34,10 +35,8 @@ function updateNumber(e) {
                 clearAll();
             }
             userPicked.push(digit);
-        } else if (len == 1) {
-            userPicked[0] += digit;
-        } else if (len == 3) {
-            userPicked[2] += digit;
+        } else if (len == 1 || len == 3) {
+            userPicked[len - 1] += digit;
         }
         updateDisplay();
     }
@@ -60,19 +59,16 @@ function updateOperator(e) {
     }
 }
 
-function getResult() {
+function addDecimal() {
     const len = userPicked.length;
-    if (len == 3){
-        const result = operate(...userPicked);
-        clearAll();
-        userPicked.push(result);
-        updateDisplay();
-        userPicked.push('Last button was =');
+    if (len == 1 || len == 3) {
+        if (!userPicked[len - 1].includes('.')) {
+            userPicked[len - 1] += '.';
+            updateDisplay();
+        }
+    } else {
+        userPicked.push('.');
     }
-}
-
-function clearAll() {
-    userPicked.length = 0;
 }
 
 function operate(num1, op, num2) {
@@ -84,20 +80,47 @@ function operate(num1, op, num2) {
         case '*':
             return num1 * num2;
         case '/':
-            if (num2 == 0) return "Undefined";
+            if (num2 == 0) return 'Undefined';
             return num1 / num2;
     }
 }
 
+function getResult() {
+    const len = userPicked.length;
+    if (len == 3){
+        const result = operate(...userPicked);
+        clearAll();
+        userPicked.push(result);
+        updateDisplay();
+        // Fill operator slot with this message
+        userPicked.push('Last button was =');
+    }
+}
+
+function clearAll() {
+    userPicked.length = 0;
+}
+
+function clearOne() {
+    const len = userPicked.length;
+    if (len == 0) return;
+    if (userPicked[len - 1].length == 1) {
+        userPicked.pop();
+    } else {
+        userPicked[len - 1] = userPicked[len - 1].slice(0, -1);
+    }
+    updateDisplay();
+}
+
 function updateDisplay() {
     output.textContent = userPicked.map(
-        str => roundIfNumber(str)).join(' ');
+            str => roundIfNumber(str)).join(' ');
     display.appendChild(output);
 }
 
 function roundIfNumber(str) {
-    if (!Number.isNaN(+str)) {
-        return Math.round(str * 100) / 100;
+    if ((!Number.isNaN(+str))) {
+        return Math.round(str * 10000) / 10000;
     }
     return str;
 }
